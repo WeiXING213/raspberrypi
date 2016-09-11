d0ac9818b4ba97300147071e2905e3941e06d0a4
xingsir@gmail.com <xingsir@gmail.com>
2016-09-11 10:46:28 +0200

    add gpio cpp test

---
+++ cpp_gpio_test.cpp
@@ -0,0 +1,27 @@
+/*
+gcc c_gpio_test.c -o blink -l wirlingPi
+*/
+#include <stdio.h>
+#include <wiringPi.h>
+
+#define LED 17  /*wiringPi pin number O, position on GPIO header 11*/
+
+using namespace std;
+
+int main(void){
+  printf ("Raspberry Pi blink\n");
+  wiringPiSetupGpio();
+  pinMode(LED, OUTPUT);
+  
+  int i;
+  for(i=0; i<5; i++){
+    digitalWrite (LED, HIGH);
+    delay (500);
+    digitalWrite (LED, LOW);
+    delay (500);
+
+  }
+  return 0;
+}
+
+
