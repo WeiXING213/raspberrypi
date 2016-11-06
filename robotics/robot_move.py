44a57619ce1351170fd5e31c66366d5c99b28ed1
xingsir@gmail.com <xingsir@gmail.com>
2016-11-06 18:45:14 +0100

    combine two

--- robotics/robot_move.py
+++ robotics/robot_move.py
@@ -3,25 +3,29 @@ import time
 import sys
 import readchar
 
-pwm2 = 0
-pwm2_value = 50
+pwm_value = 90
+pwm = 0
 def init_servo():
-   global pwm2
-   global pwm2_value
-
+   global pwm_value
+   global pwm
    gpio.setmode(gpio.BOARD)
    gpio.setup(12, gpio.OUT)
-   pwm2 = gpio.PWM(12, pwm2_value)
-   pwm2.start(5)
+   pwm = gpio.PWM(12, 50)
+   
+   duty = float(pwm_value)/(180/(12.5-2.5)) + 2.5
+   pwm.start(duty)
 
+def active_servo():
+   gpio.setmode(gpio.BOARD)
+   gpio.setup(12, gpio.OUT)
+ 
 def init():
     gpio.setmode(gpio.BOARD)
-    """
     gpio.setup(7, gpio.OUT)
     gpio.setup(11, gpio.OUT)
     gpio.setup(13, gpio.OUT)
     gpio.setup(15, gpio.OUT)
-    """
+    
 def forward(tf):
     gpio.output(7, False)
     gpio.output(11, True)
@@ -78,32 +82,39 @@ def change_duty(pwm, angle):
     pwm.ChangeDutyCycle(duty)
 
 def key_input(key_press):
-    init()
     sleep_time = 0.2
-    global pwm2
-    global pwm2_value
+    global pwm
+    global pwm_value
 
     if key_press.lower() == 'z':
+        init()
         forward(sleep_time)
     elif key_press.lower() == 's':
+        init()
         reverse(sleep_time)
     elif key_press.lower() == 'q':
+        init()
         turn_left(sleep_time)
     elif key_press.lower() == 'd':
+        init()
         turn_right(sleep_time)
     elif key_press.lower() == 'a':
+        init()
         pivot_left(sleep_time)
     elif key_press.lower() == 'e':
+        init()
         pivot_right(sleep_time)
+    #--servo--
     elif key_press.lower() == 'i':
-        pwm2_value -= 5
-        change_duty(pwm2, pwm2_value)
+        active_servo()
+        pwm_value -= 5
+        change_duty(pwm, pwm_value)
     elif key_press.lower() == 'k':
-        pwm2_value += 5
-        change_duty(pwm2, pwm2_value)
+        pwm_value += 5
+        change_duty(pwm,pwm_value)
     elif key_press.lower() == 'p':
         gpio.cleanup()
-        pwm2.stop()
+        pwm.stop()
         sys.exit(0);
 print "-------------------------------------------------------------------------------------------------------------------"
 print "car : z - forward | s - reverse | q - turn left | d - turn right | a - turn left | e - pivot right | i - pivot left"
@@ -111,8 +122,6 @@ print "camera : i - look up | k - look down | o - look left | m - look right"
 print "\np - exit"
 print "-------------------------------------------------------------------------------------------------------------------"
 
-
 init_servo()
 while True:
-    init()
     key_input(readchar.readkey())
